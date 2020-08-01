022c24a94c34812c27d09cc20a02f12b6c738f34
chengweiping <1623405873@qq.com>
2020-08-01 17:06:20 +0800

    更新

--- src/main/java/com/cwp/CwpServiceAutoConfiguration.java
+++ src/main/java/com/cwp/CwpServiceAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.cwp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,8 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @ConditionalOnClass(CwpService.class)  //当CwpService.class 在类路径中时生效
+//当配置文件中存在cwp.enabled=open的配置值时生效，如果没配置这个属性值时也生效
+@ConditionalOnProperty(prefix = "cwp",name = "enabled",havingValue = "open",matchIfMissing = true)
 @EnableConfigurationProperties(CwpProperties.class)//开启配置属性注解,导入CwpProperties类，生成对应的bean
 public class CwpServiceAutoConfiguration {
 
